3f1f858a1580746594d05cf22e0aa422888a217b
Vudutha Sai Praneetham <vuduthasaipraneetham@gmail.com>
2025-09-23 00:29:02 +0530

    Removed the lines that drops and creates the database and included the bcrypt library to encrypt the user passwords

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db=SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SERECT_KEY']='supersecretkey'
+    app.config['SECRET_KEY']='supersecretkey'
     app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///guessword.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
     db.init_app(app)
+    bcrypt.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
-
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
 
     return app
 
